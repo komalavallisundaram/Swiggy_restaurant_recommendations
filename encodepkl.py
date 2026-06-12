a41196e3b11fbd4cc0ad1050c0a66108c93aa2c4
komalavallisundaram <komalsuya@gmail.com>
2026-06-12 16:28:16 +0530

    Delete encodepkl.py

--- encodepkl.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pickle
-
-encoded_df = pd.read_csv(r"C:/Users/shenbagam/Downloads/swiggy/encoder.csv")
-
-with open(r"C:/Users/shenbagam/Downloads/swiggy/encoder.pkl","wb") as f:
-    pickle.dump(encoded_df, f)
-
-print("✅ Encoder PKL saved")
