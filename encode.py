73ebe2658b382e2e482b1d79e785d87b03143b1d
komalavallisundaram <komalsuya@gmail.com>
2026-06-12 16:28:04 +0530

    encode.py

--- encode.py
+++ encode.py
@@ -4,7 +4,7 @@ import joblib
 print("🔄 Starting encoding process...")
 
 cleaned_path = r"C:/Users/shenbagam/Downloads/swiggy/cleaneddata.csv"
-print("📂 Reading cleaneddata.csv...")
+print("C:/Users/shenbagam/Downloads/swiggy/cleaneddata.csv")
 cleaned_df = pd.read_csv(cleaned_path)
 
 cleaned_df['cuisine'] = cleaned_df['cuisine'].astype(str).str.lower()
@@ -14,16 +14,13 @@ cleaned_df['cuisine'] = cleaned_df['cuisine'].str.strip()
 
 categorical_cols = ['city', 'cuisine']
 encoded_cats = pd.get_dummies(cleaned_df[categorical_cols], drop_first=False)
-
 encoded_df = pd.concat(
     [cleaned_df[['id', 'name']], encoded_cats, cleaned_df[['rating', 'cost']]],
     axis=1
 )
-
 encoded_csv_path = r"C:/Users/shenbagam/Downloads/swiggy/encoder.csv"
 encoded_pkl_path = r"C:/Users/shenbagam/Downloads/swiggy/encoder.pkl"
 
 encoded_df.to_csv(encoded_csv_path, index=False)
 joblib.dump(encoded_df, encoded_pkl_path, compress=3)
-
 print("✅ Encoder CSV + PKL saved successfully!")
